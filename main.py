161952d30d922f2311167fccaeaede411b37ba89
A-Haider13 <97507694+A-Haider13@users.noreply.github.com>
2023-05-24 05:20:42 +0500

    The character is added and there is movement. Made a new folder and added the images into that

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from BackgroundScreen import Background
 from runner import runner
+import sys
 
 pygame.init()
 
@@ -11,20 +12,19 @@ pygame.display.set_caption("Infinite Runner: Phew Phew")
 
 clock = pygame.time.Clock()
 
-sprite_sheet_image = pygame.image.load("maincharacter.jpg")
-background_image = pygame.image.load("background.jpg")
+# sprite_sheet_image = pygame.image.load("maincharacter.jpg")
+background_image = pygame.image.load("images/background.jpg")
 background_rect = background_image.get_rect()
 background_x = 0
 
 BackGround = Background(background_image, [0, 0])
-Runner = runner()
 
-frame_width = 64
-frame_height = 64
-
-for x in range(0, sprite_sheet_image.get_width(), frame_width):
-    frame = sprite_sheet_image.subsurface(pygame.Rect(x, 0, frame_width, frame_height))
-    Runner.frames.append(frame)
+Runner = runner()  # spawn player
+Runner.rect.x = 100  # go to x
+Runner.rect.y = 500  # go to y
+player_list = pygame.sprite.Group()
+player_list.add(Runner)
+steps = 7
 
 game_is_running = True
 
@@ -35,17 +35,38 @@ while game_is_running:
         if event.type == QUIT:
             game_is_running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord('q'):
+                pygame.quit()
+                try:
+                    sys.exit()
+                finally:
+                    main = False
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                Runner.control(-steps, 0)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                Runner.control(steps, 0)
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                Runner.jump()
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                Runner.control(steps, 0)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                Runner.control(-steps, 0)
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                Runner.control(0, steps * 2)
+
     background_x -= 3
 
     if background_x <= -background_rect.width:
         background_x = 0
 
-    runner.update()
-
     screen.fill((0, 0, 0))
     screen.blit(background_image, (background_x, 0))
-    screen.blit(Runner.image, Runner.rect)
     screen.blit(background_image, (background_x + background_rect.width, 0))
+    Runner.update()
+    player_list.draw(screen)
 
     pygame.display.flip()
 
--- runner.py
+++ runner.py
@@ -1,25 +1,53 @@
-import pygame 
-import sys 
+import pygame
+import sys
 from pygame.locals import *
+import os
 
 class runner(pygame.sprite.Sprite):
-    frames = [] 
-        
+    
     def __init__(self):
-        super().__init__()
-        self.currentframe = 0
-        self.animationstate = "running"
-        self.image = runner.frames[self.currentframe]
-        self.rect = self.image.get_rect()
-        self.rect.left = 0  # Starting position of the character
-        self.rect.top = 600 // 2 # Starting position of the character and 600 because that is the screen height
+        ALPHA = (0, 255, 0)
+        pygame.sprite.Sprite.__init__(self)
+        self.movex = 0 # move along X
+        self.movey = 0 # move along Y
+        self.frame = 0 # count frames
+        self.images = []
+        for i in range(1, 5):
+            img = pygame.image.load(os.path.join('images', 'hero' + str(i) + '.png')).convert_alpha()
+            # Scale the image to the desired size
+            img = pygame.transform.scale(img, (100, 100))  # Adjust the size as needed
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+        self.gravity = 0.9  # Gravity value
+        self.jump_power = -20  # Initial jump power
 
     def update(self):
-        if self.animationstate == "running":
-            self.currentframe += 1
-            if self.currentframe >= len(runner.frames):
-                self.currentframe = 0
+        self.apply_gravity()  # Apply gravity to vertical movement
+        self.rect.x += self.movex # move along X
+        self.rect.y += self.movey # move along Y
 
-        self.image = runner.frames[self.current_frame]
+        ani = 4 # animation cycles
+        # moving left
+        if self.movex < 0:
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = pygame.transform.flip(self.images[self.frame // ani], True, False)
 
+        # moving right
+        if self.movex > 0:
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = self.images[self.frame//ani]
+    
+    def control(self,x,y):
+        self.movex += x 
+        self.movey += y
 
+    def apply_gravity(self):
+        self.movey += self.gravity
+
+    def jump(self):
+        self.movey = self.jump_power
