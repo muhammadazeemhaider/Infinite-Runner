b41e9449f34ef9fa8b42c4b10781c5c954aa4f76
A-Haider13 <97507694+A-Haider13@users.noreply.github.com>
2023-05-25 19:46:06 +0500

    updated

---
+++ enemy.py
@@ -0,0 +1,8 @@
+import pygame 
+import sys 
+from pygame.locals import *
+
+class enemy(pygame.sprite.Sprite):
+    def __init__():
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/goblinHead.png")
